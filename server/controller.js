33b9655f2b962c8534276907d0d199d33761b94a
Austin Holmes <austin.holmes4@gmail.com>
2022-10-21 09:58:47 -0600

    first feature complete

---
+++ server/controller.js
@@ -0,0 +1,22 @@
+module.exports = {
+
+    getCompliment: (req, res) => {
+        const compliments = ["Gee, you're a smart cookie!", "Cool shirt!", "Your Javascript skills are stellar."];
+      
+        // choose random compliment
+        let randomIndex = Math.floor(Math.random() * compliments.length);
+        let randomCompliment = compliments[randomIndex];
+      
+        res.status(200).send(randomCompliment);
+    },
+
+    getFortune: (req, res) => {
+        const fortunes = ['It will be a good day', 'It will be a bad day', 'You will succeed', 'You will not succeed'];
+
+        let randomIndex = Math.floor(Math.random() * fortunes.length);
+        let randomFortune = fortunes[randomIndex];
+
+        res.status(200).send(randomFortune);
+    }
+
+}
